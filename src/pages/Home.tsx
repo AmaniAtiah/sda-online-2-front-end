7b79f53c9851bbefee80222a3869106e63884807
Amaniatiah <amaniatiah@gmail.com>
2024-05-29 09:31:44 +0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,17 @@ export const Home = () => {
     <div>
       <div className="main-container flex-space-around">
         <PageTitle title="Home" />
+        <div className="hero">
+          <p>
+            We provide the finest coffee crops, committed to perfection, always striving for
+            excellence and uniqueness, keeping in mind the achievement of our customers’ desires and
+            goals by roasting coffee to high standards so that every customer has an unforgettable
+            coffee experience.
+          </p>
+          <a href="#" className="button__hero">
+            Learn More
+          </a>
+        </div>
         {/* <div className="sidebar-container">
           <ProductSidebar />
         </div> */}
